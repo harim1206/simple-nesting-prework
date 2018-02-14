496eb26655d46e7127993208ac2a94f5980607f0
Harim Choi <harim1206@gmail.com>
2018-02-14 20:32:10 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -64,11 +64,7 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
-# }
+
 
 	programmer_hash =
  		{
@@ -86,6 +82,13 @@ def adding_matz
         }
      }
 
+		 # add the following information to the top level of programmer_hash
+		 # :yukihiro_matsumoto => {
+		 #   :known_for => "Ruby",
+		 #    :languages => ["LISP, C"]
+		 # }
+		 programmer_hash[:yukihiro_matsumoto][:known_for] => "Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:languages] => ["LISP, C"]
 
 end
 
