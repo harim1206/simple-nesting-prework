a6e90cb87334262271cb17cd0a83000f391ad613
Harim Choi <harim1206@gmail.com>
2018-02-14 21:24:54 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -88,14 +88,15 @@ def adding_matz
 		 #    :languages => ["LISP, C"]
 		 # }
 		 programmer_hash[:yukihiro_matsumoto] = {
-			 :known_for => "Ruby"
-		 }
-
-		 programmer_hash[:yukihiro_matsumoto] = {
 			 :languages => ["LISP, C"]
 
 		 }
 
+		 programmer_hash[:yukihiro_matsumoto] = {
+			 :known_for => "Ruby"
+		 }
+
+		 
 end
 
 def changing_alan
