184fa9a9cbcef86155a85dba410430684d792e42
Harim Choi <harim1206@gmail.com>
2018-02-14 20:59:50 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -117,7 +117,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 puts("ATTENTION: #{programmer_hash[:alan_kay][:known_for]}")
-		 programmer_hash[:alan_kay]{
+		 programmer_hash[:alan_kay] => {
 			 :known_for => "GUI"
 
 			}
